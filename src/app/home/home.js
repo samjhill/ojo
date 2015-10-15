1b5fcdef1b7662d53be98a7d36c8db0d9f86b180
Sam Hill <samuhill@gmail.com>
2015-10-15 18:45:11 -0400

    add more calls & begin notification bar

--- src/app/home/home.js
+++ src/app/home/home.js
@@ -17,18 +17,46 @@ angular.module( 'ngBoilerplate.home', [
   });
 })
 
-.controller( 'HomeCtrl', function HomeController( $scope, $http ) {
-  
-    $http.get("http://samhillmade.it:4730/systemLoad")
+.controller( 'HomeCtrl', function HomeController( $scope, $http, $timeout ) {
+    
+    $scope.getStatus = function(){
+      $http.get("http://samhillmade.it:4730/systemLoad")
         .success(function(response) {
           $scope.systemLoad = response;
-    });
-        
-    $http.get("http://samhillmade.it:4730/status")
-        .success(function(response) {
-          response.players = response.players.split('/');
-          $scope.systemStatus = response;
-    });
+      });
+          
+      $http.get("http://samhillmade.it:4730/status")
+          .success(function(response) {
+            response.players = response.players.split('/');
+            $scope.systemStatus = response;
+      });
+    };
+ 
+    $scope.startServer = function(){
+      $parent.$scope.message.type = 'info';
+      $parent.$scope.message.data = 'Server is starting up...';
+      
+      $http.post("http://samhillmade.it:4730/start")
+          .success(function(response) {
+            $parent.$scope.message.type = 'success';
+            $parent.$scope.message.data = response;
+      });
+    };
+    
+    $scope.stopServer = function(){
+      $parent.$scope.message.type = 'info';
+      $parent.$scope.message.data = 'Server is shutting down...';
+      
+      $http.post("http://samhillmade.it:4730/stop")
+          .success(function(response) {
+            $parent.$scope.message.type = 'success';
+            $parent.$scope.message.data = response;
+      });
+    };
+    
+    $timeout($scope.getStatus, 5000); //update every 5 seconds
+    
+    $scope.getStatus(); //one for the initial load
       
     //$http.get("http://samhillmade.it:4730/checkUpdate")
     //    .success(function(response) {
